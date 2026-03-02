31077943a5be0f8ccc05e8acf3644d5f061589f9
Jacob <jacobdhartzell@gmail.com>
2026-03-02 13:00:51 -0500

    func added

--- js06a.js
+++ js06a.js
@@ -15,5 +15,23 @@ window.addEventListener("load", function() {
       let model = orderForm.elements.model;
 
       model.focus();
+      calcOrder();
 });
 
+
+function calcOrder() {
+      let mIndex = model.selectedIndex;
+      let mValue = model.options[mIndex].value;
+
+      let qIndex = orderForm.elements.qty.selectedIndex;
+      let quantity = orderForm.elements.qty[qIndex].value;
+
+      let modelCost = mValue * quantity;
+      orderForm.elements.modelCost.value = modelCost;
+
+      let planValue = document.querySelector('input[name="plan"]:checked').value;
+      let planCost = planValue * quantity;
+      orderForm.elements.planCost.value = planCost;
+
+
+}
